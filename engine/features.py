c6f3e00f1352bda14eababcead72e77063c3a2c6
ANSH KUMAR <anshkumar1399@gmail.com>
2025-05-18 22:56:00 +0530

    navigate

--- engine/features.py
+++ engine/features.py
@@ -1,8 +1,9 @@
 
 from playsound import playsound
 import os
+import eel
 
-
+@eel.expose
 # Playing assiatnt sound function
 def playAssistantSound():
     music_dir = os.path.join("www", "assets", "audio", "start_sound.mp3")
--- www/main.js
+++ www/main.js
@@ -37,4 +37,13 @@ $(document).ready(function () {
 
     });
 
+    // mic button click event
+
+    $("#MicBtn").click(function () {
+        eel.playAssistantSound()
+        $("#Oval").attr("hidden", true);
+        $("#SiriWave").attr("hidden", false);
+
+    });
+
 });
